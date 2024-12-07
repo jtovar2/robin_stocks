29d141906e7d61be7ffc4467a8560b0491fcd953
Javier Tovar <javier@Javiers-MacBook-Pro.local>
2024-12-07 15:10:42 -0700

    new type of verification type to handle prompt

--- robin_stocks/robinhood/authentication.py
+++ robin_stocks/robinhood/authentication.py
@@ -297,9 +297,14 @@ def create_session_on_db(username=None, password=None, expiresIn=691200, scope='
             resp = pathfind_user_machine(payload['device_token'], challenge_id)
 
             user_view = pathfind_user_view(resp['id'])
+            is_prompt = False
             if user_view['context']['sheriff_challenge']['type'] == 'prompt':
                 check_prompt_approved(user_view['context']['sheriff_challenge']['id'])
-            resp_obj = {'verification_type': 'challenge', 'user_view_id': resp['id'] ,'challenge_id': user_view['context']['sheriff_challenge']['id'], 'payload': payload, 'url': url}
+                is_prompt = True
+            verification_type = 'challenge'
+            if is_prompt:
+                verification_type = 'prompt'
+            resp_obj = {'verification_type':verification_type, 'user_view_id': resp['id'] ,'challenge_id': user_view['context']['sheriff_challenge']['id'], 'payload': payload, 'url': url, 'is_prompt': is_prompt}
             return resp_obj
 
         elif 'challenge' in data:
